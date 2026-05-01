b827799d1e3cfe8fa999aba5917d7a89d7416fb3
Sid. <siddhantg2002@gmail.com>
2026-05-01 12:20:27 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,29 @@
+# from collections import Counter
+
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return Counter(s) == Counter(t)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        freq = {}
+        
+        # Count characters from s
+        for ch in s:
+            if ch in freq:
+                freq[ch] += 1
+            else:
+                freq[ch] = 1
+        
+        # Subtract using t
+        for ch in t:
+            if ch not in freq:
+                return False
+            freq[ch] -= 1
+            if freq[ch] < 0:
+                return False
+        
+        return True
